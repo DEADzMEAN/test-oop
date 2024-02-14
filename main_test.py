3346d6f34f70b0a6849b1d89915fd1870f9a90b9
deadzmean <deadzmean@gmail.com>
2024-02-15 00:11:54 +0400

    ver 11 15.02.2024

---
+++ main_test.py
@@ -0,0 +1,6 @@
+import main
+import unittest
+
+class Test_main(unittest.TestCase):
+    def test_checked_input_true(self):
+
---
+++ support_test.py
@@ -0,0 +1,10 @@
+from support import Support
+import unittest
+
+class TestSupport(unittest.TestCase):
+    def heal_test_true(self, other):
+        me: Support = Support(name="me", hp=50, defence=15, speed=15, attack_speed=10, heal_power=1, buff=1, power=1)
+        other: Support = Support(name="Other", hp=50, defence=15, speed=15, attack_speed=10, heal_power=1, buff=1, power=1)
+        hp_before = other.hp
+        me.heal(other)
+        self.assertGreater(other.hp, hp_before)
