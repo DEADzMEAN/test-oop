c6cbaeec27346765b95b07e968261abe3172c01e
Momotoff <momotofff@mail.ru>
2024-03-15 20:24:24 +0400

    Fix test

--- test/test_tank.py
+++ test/test_tank.py
@@ -7,8 +7,7 @@ me: Tank = Tank(name='Василий', hp=500, defence=200, speed=18, attack_spe
 class TestTank(unittest.TestCase):
     def test_get_dpm_against_tank_damage_expected(self):
         other: Tank = Tank(name='Other tank', hp=500, defence=200, speed=18, attack_speed=20, aggression=1, power=9)
-#        self.assertEqual(me.get_dpm(other), 0.045, "Внезапно не тот дамаг")
-        self.assertEqual(me.get_dpm(other), 0.05, "Внезапно не тот дамаг")
+        self.assertEqual(me.get_dpm(other), 0.045, "Внезапно не тот дамаг")
 
 #    def test_failed(self):
 #        self.assertTrue(False)
