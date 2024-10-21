bc4333987758317a6f07fe469cf6aecf1ec38ecc
deadzmean <deadzmean@gmail.com>
2024-10-21 22:24:22 +0400

    fix 5 input_file*

--- utils/symbol_checker.py
+++ utils/symbol_checker.py
@@ -25,15 +25,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Config files checker")
     parser.add_argument("--input_dir=/", type=str, help="Input directory", required=True)
     parser.add_argument("--mask=*.yml", type=str, help="Input mask", required=True)
+    parser.add_argument("--input_file", type=str, help="File name", required=True)
 
     # TODO: Replace --input_file with --input_dir, --mask, --recursive to do it like:
     # TODO: symbol_checker.py --input_dir=/.github/workflows --mask=*.yml --recursive
+
 
     args = parser.parse_args()
     logging.info(f"Input file: {args.input_file}")
     path = '.github/workflows'
     valid: bool = True
-    input_file = 'file1'
+
     for input_file in glob.glob(os.path.join(path, '**/*.yml')):   #чтобы не забыть , recursive=True не дружит с чётким путём но я оставил **/*yml
         with open(os.path.join(os.getcwd(), input_file), 'r', encoding='UTF-8') as f:
             index: int = 0
