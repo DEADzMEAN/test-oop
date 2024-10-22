ee9ebad1775726ff36af257ac298db00dfb3b704
deadxmean <deadzmean@gmail.com>
2024-10-22 13:10:04 +0400

    fix 8 in work

--- utils/symbol_checker.py
+++ utils/symbol_checker.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-import logging
 import argparse
-import os
 import glob
-
-logging.basicConfig(level=logging.INFO, format="%(asctime)s SymbolsChecker %(levelname)s %(message)s")
-
+import logging
+import os
 
 def is_line_valid(data: str, number: int) -> bool:
     result: bool = True
@@ -20,23 +17,31 @@ def is_line_valid(data: str, number: int) -> bool:
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     logging.info("Started")
 
     parser = argparse.ArgumentParser(description="Config files checker")
-    parser.add_argument("--input_dir=/", type=str, help="Input directory", required=True)
-    parser.add_argument("--mask=*.yml", type=str, help="Input mask", required=True)
+    parser.add_argument("--input_dir", type=str, help="Input directory", required=True)
+    parser.add_argument("--mask", type=str, help="Input mask", required=True)
+    parser.add_argument("--recursive", action='store_true', help="Recursively search for files")
 
     # TODO: Replace --input_file with --input_dir, --mask, --recursive to do it like:
     # TODO: symbol_checker.py --input_dir=/.github/workflows --mask=*.yml --recursive
 
-
     args = parser.parse_args()
-    logging.info(f"Input file: {args.input_file}")
-    path = '.github/workflows'
+
+    path = args.input_dir
+    mask = args.mask
+    recursive = args.recursive
+
+    logging.info(f"Searching in directory: {path} with mask: {mask}")
+
+    search_pattern = os.path.join(path, '**', mask) if recursive else os.path.join(path, mask)
     valid: bool = True
 
-    for input_file in glob.glob(os.path.join(path, '**/*.yml')):   #чтобы не забыть , recursive=True не дружит с чётким путём но я оставил **/*yml
-        with open(os.path.join(os.getcwd(), input_file), 'r', encoding='UTF-8') as f:
+    for input_file in glob.glob(search_pattern, recursive=recursive):
+        logging.info(f"Checking file: {input_file}")
+        with open(input_file, 'r', encoding='UTF-8') as f:
             index: int = 0
             for line in f:
                 index += 1
@@ -48,6 +53,4 @@ if __name__ == "__main__":
         exit(1)
 
     logging.info("No errors found, you're breathtaking!")
-#    with open(args.input_file, 'r', encoding='UTF-8') as f:
-
 
